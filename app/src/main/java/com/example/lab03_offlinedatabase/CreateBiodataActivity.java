8e0fe3bc993524173e9f28d4c4690f848f99407b
Fabian Harrison <2023622802@student.uitm.edu.my>
2023-11-26 15:11:28 +0800

    - Up to Slide 28

--- app/src/main/java/com/example/lab03_offlinedatabase/CreateBiodataActivity.java
+++ app/src/main/java/com/example/lab03_offlinedatabase/CreateBiodataActivity.java
@@ -2,10 +2,17 @@ package com.example.lab03_offlinedatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateBiodataActivity extends AppCompatActivity {
 
+    protected Cursor cursor;
+    DataHelper dbHelper;
+    Button ton1, ton2;
+    EditText text1, text2, text3, text4, text5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
